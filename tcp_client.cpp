e2926af0da3e1f75df018e66c08bb65fbdf318fd
Rafael Wang <rafaelwa@usc.edu>
2025-09-13 21:04:47 -0700

    Update tcp_client.cpp

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -15,14 +15,13 @@ int main(int argc, char const *argv[])
 	// check to see if user input is valid
 	char socket_read_buffer[1024];
 	
-	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
+	std::string server_ip = "localhost"; //address to connect to
+	std::string server_port = "6666";
 
 	int opt = 1;
 	int client_fd = -1;
-
-	// TODO: Create a TCP socket()
+ 
+	client_fd = socket(AF_INET, SOCK_STREAM, 0); //creating the client socket
 
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
@@ -43,11 +42,23 @@ int main(int argc, char const *argv[])
 	hints.ai_socktype = SOCK_STREAM;
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
-	// TODO: Connect() to the server (hint: you'll need to use server_addr)
-	// TODO: Retreive user input
-	// TODO: Send() the user input to the server
-	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
-	// TODO: Close() the socket
+	connect(client_fd, (*server_addr).ai_addr, (*server_addr).ai_addrlen); //connecting to the server with parameters of the client socket, the server's socket address, and its length 
+
+	char buffer[256];
+	std::cout << "Type input: " << std::endl; //get the user input
+	std::cin.getline(buffer, 256);
+	if (std::cin.fail()){
+		return -1;
+	}
+
+	send(client_fd, buffer, strlen(buffer), 0); //sending the data to the server
+
+	int n;
+	n = read(client_fd, socket_read_buffer, 1023);
+	socket_read_buffer[n] = '\0';
+	std::cout << socket_read_buffer << std::endl;
+
+	close(client_fd); //closing the client socket
 
 	return 0; 
 } 
